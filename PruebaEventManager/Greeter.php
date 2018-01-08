7e157b50bf7332e4d027fdee7a9be8128d40c0d6
Kevin Juárez <Kevin@HP14>
2018-01-07 22:05:29 -0600

    prueba greet.php

--- PruebaEventManager/Greeter.php
+++ PruebaEventManager/Greeter.php
@@ -1,1 +1,38 @@
+<?php  
+require __DIR__ . '/vendor/autoload.php';  
 
+use Zend\EventManager\EventManagerInterface; 
+use Zend\EventManager\EventManager; 
+use Zend\EventManager\EventManagerAwareInterface; 
+
+class Greeter implements EventManagerAwareInterface { 
+   protected $events;
+   public function setEventManager(EventManagerInterface $events) { 
+      $events->setIdentifiers([__CLASS__, get_called_class(), ]); 
+      $this->events = $events; 
+      return $this; 
+   }  
+   public function getEventManager() { 
+      if (null === $this->events) { 
+         $this->setEventManager(new EventManager()); 
+      } 
+      return $this->events; 
+   } 
+   public function greet($message) { 
+      printf("\"%s\" from class\n", $message); 
+      $this->getEventManager()->trigger(__FUNCTION__, $this, [$message ]); 
+   } 
+} 
+
+$greeter = new Greeter(); 
+$greeter->greet("Hello");  
+$greeter->getEventManager()->attach('greet', function($e) { 
+   $event_name = $e->getName(); 
+   $target_name = get_class($e->getTarget()); 
+   $params_json = json_encode($e->getParams()); 
+   printf("\"%s\" event of class \"%s\" is called." . " The parameter supplied is %s\n",
+      $event_name,
+      $target_name,  
+      $params_json); 
+});  
+$greeter->greet("Hello"); 
