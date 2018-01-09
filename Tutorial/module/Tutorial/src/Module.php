960b4b96b6aa397648ae8331715b7e7d89b5e68c
Kevin Juárez <Kevin@HP14>
2018-01-08 22:11:25 -0600

    asdfasdf

--- Tutorial/module/Tutorial/src/Module.php
+++ Tutorial/module/Tutorial/src/Module.php
@@ -3,6 +3,6 @@ namespace Tutorial;
 use Zend\ModuleManager\Feature\ConfigProviderInterface;
 class Module implements ConfigProviderInterface { 
    public function getConfig() {    
-      return include __DIR__ . '/../config/module.config.php'; 
+      return include __DIR__ . '/../config/module.config.php'; //edicion
    }    
 } 
