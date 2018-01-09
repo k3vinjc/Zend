ed3c527a13c63d3e57922f1d5f1dfda04562e67c
Kevin Juárez <Kevin@HP14>
2018-01-08 21:48:29 -0600

    Modulo

---
+++ Tutorial/module/Tutorial/src/Module.php
@@ -0,0 +1,8 @@
+<?php  
+namespace Tutorial; 
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+class Module implements ConfigProviderInterface { 
+   public function getConfig() {    
+      return include __DIR__ . '/../config/module.config.php'; 
+   }    
+} 
