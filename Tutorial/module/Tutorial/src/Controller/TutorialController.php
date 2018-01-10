0e5b154a0037d47dbb021f78ece77830c73d471b
Kevin Juárez <Kevin@HP14>
2018-01-09 22:30:19 -0600

    View & Controller

--- Tutorial/module/Tutorial/src/Controller/TutorialController.php
+++ Tutorial/module/Tutorial/src/Controller/TutorialController.php
@@ -1,7 +1,7 @@
 <?php
 namespace Tutorial\TutoriallController;
-//use Zend\Mvc\Controller\AbstractActionController; 
-//use Zend\View\Model\ViewModel; 
+use Zend\Mvc\Controller\AbstractActionController; 
+use Zend\View\Model\ViewModel; 
 class TutorialController extends AbstractActionController{
     public function indexAction(){
         return ViewModel();
