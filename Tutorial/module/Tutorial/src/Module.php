e8f90475cae2a597626e653669f1763875c0b49e
Kevin Juárez <Kevin@HP14>
2018-01-10 16:30:26 -0600

    View & Controller

--- Tutorial/module/Tutorial/src/Module.php
+++ Tutorial/module/Tutorial/src/Module.php
@@ -2,10 +2,9 @@
 
 namespace Tutorial;
 
-use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
 use Zend\ModuleManager\Feature\ConfigProviderInterface;
 
-class Module implements AutoloaderProviderInterface, ConfigProviderInterface {
+class Module implements ConfigProviderInterface {
 
     public function getConfig() {
         return include __DIR__ . '/../config/module.config.php'; //edicion
