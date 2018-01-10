8beeb53bdceef8d8f01a081c8a35b421b2279a47
Kevin Juárez <Kevin@HP14>
2018-01-10 16:17:40 -0600

    View & Controller

--- Tutorial/module/Tutorial/src/Module.php
+++ Tutorial/module/Tutorial/src/Module.php
@@ -1,8 +1,27 @@
-<?php  
-namespace Tutorial; 
+<?php
+
+namespace Tutorial;
+
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
 use Zend\ModuleManager\Feature\ConfigProviderInterface;
-class Module implements ConfigProviderInterface { 
-   public function getConfig() {    
-      return include __DIR__ . '/../config/module.config.php'; //edicion
-   }    
-} 
+
+class Module implements AutoloaderProviderInterface, ConfigProviderInterface {
+
+    public function getAutoloaderConfig() {
+        return array(
+            'Zend\Loader\ClassMapAutoloader' => array(
+                __DIR__ . '/autoload_classmap.php',
+            ),
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+
+    public function getConfig() {
+        return include __DIR__ . '/../config/module.config.php'; //edicion
+    }
+
+}
