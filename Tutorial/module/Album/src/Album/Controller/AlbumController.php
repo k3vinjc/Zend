ee3165fbfab820d1a05f5e6250c6da4054d02f3b
Kevin Juárez <Kevin@HP14>
2018-01-10 23:40:09 -0600

    Album

--- Tutorial/module/Album/src/Album/Controller/AlbumController.php
+++ Tutorial/module/Album/src/Album/Controller/AlbumController.php
@@ -12,8 +12,12 @@
 
  class AlbumController extends AbstractActionController
  {
+      protected $albumTable;
      public function indexAction()
      {
+          return new ViewModel(array(
+             'albums' => $this->getAlbumTable()->fetchAll(),
+         ));
      }
 
      public function addAction()
@@ -26,5 +30,14 @@
 
      public function deleteAction()
      {
+     }
+     
+      public function getAlbumTable()
+     {
+         if (!$this->albumTable) {
+             $sm = $this->getServiceLocator();
+             $this->albumTable = $sm->get('Album\Model\AlbumTable');
+         }
+         return $this->albumTable;
      }
  }
