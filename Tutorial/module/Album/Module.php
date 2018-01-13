0c4842e8a9337104f651277fb980de9f16feeab6
Kevin Juárez <Kevin@HP14>
2018-01-12 21:22:54 -0600

    Actualización de Album a ZF3

--- Tutorial/module/Album/Module.php
+++ Tutorial/module/Album/Module.php
@@ -15,7 +15,7 @@ use Album\Model\AlbumTable;
 use Zend\Db\ResultSet\ResultSet;
 use Zend\Db\TableGateway\TableGateway;
 
-class Module implements AutoloaderProviderInterface, ConfigProviderInterface {
+class Module implements ConfigProviderInterface {
 
     public function getConfig() {
         return include __DIR__ . '/config/module.config.php';
